da621753e1f54080dd562b214dfdfdec1d9cf9e3
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-07-26 12:40:34 +0000

    Added mapping of JAXB exceptions to responses.

---
+++ server-webapp/src/main/java/org/taverna/server/master/rest/JAXBExceptionHandler.java
@@ -0,0 +1,23 @@
+package org.taverna.server.master.rest;
+
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN_TYPE;
+import static javax.ws.rs.core.Response.Status.FORBIDDEN;
+import static org.taverna.server.master.TavernaServerImpl.log;
+import static org.taverna.server.master.TavernaServerImpl.logOutgoingExceptions;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import javax.xml.bind.JAXBException;
+
+@Provider
+public class JAXBExceptionHandler implements ExceptionMapper<JAXBException> {
+	@Override
+	public Response toResponse(JAXBException exn) {
+		if (logOutgoingExceptions)
+			log.info("converting exception to response", exn);
+		return Response.status(FORBIDDEN).type(TEXT_PLAIN_TYPE).entity(
+				"APIEpicFail: " + exn.getErrorCode() + "\n" + exn.getMessage())
+				.build();
+	}
+}
