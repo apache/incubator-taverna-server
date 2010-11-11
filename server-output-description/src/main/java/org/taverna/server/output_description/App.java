61d166cc2c3bb31b4eac827ff4aa64742a1cd81c
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-11-11 15:38:05 +0000

    Add project to contain RDF output descriptions

---
+++ server-output-description/src/main/java/org/taverna/server/output_description/App.java
@@ -0,0 +1,13 @@
+package org.taverna.server.output_description;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
