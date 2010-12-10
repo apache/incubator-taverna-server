fe7d9c5a6fa8fb49b4d8824d331aeece2c9a6fa2
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-12-10 11:39:49 +0000

    RESOLVED - issue TAVSERV-144: Server could supply a version string http://www.mygrid.org.uk/dev/issues/browse/TAVSERV-144

--- server-webapp/src/main/java/org/taverna/server/master/common/VersionedElement.java
+++ server-webapp/src/main/java/org/taverna/server/master/common/VersionedElement.java
@@ -2,8 +2,14 @@ package org.taverna.server.master.common;
 
 import static org.taverna.server.master.common.Namespaces.SERVER;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
+
+import org.taverna.server.master.TavernaServerImpl;
 
 /**
  * The type of an element that declares the version of the server that produced
@@ -16,12 +22,43 @@ public abstract class VersionedElement {
 	/** What version of server produced this element? */
 	@XmlAttribute(namespace = SERVER)
 	public String serverVersion;
-	static final String VERSION = "2.2b1-SNAPSHOT";
+	/** What revision of server produced this element? Derived from SCM commit. */
+	@XmlAttribute(namespace = SERVER)
+	public String serverRevision;
+	/** When was the server built? */
+	@XmlAttribute(namespace = SERVER)
+	public String serverBuildTimestamp;
+	static final String VERSION, REVISION, TIMESTAMP;
+	static {
+		Properties p = new Properties();
+		InputStream is = null;
+		try {
+			p.load(is = VersionedElement.class
+					.getResourceAsStream("/version.properties"));
+		} catch (IOException e) {
+			TavernaServerImpl.log.warn("failed to read /version.properties", e);
+		} finally {
+			try {
+				if (is != null)
+					is.close();
+			} catch (IOException e) {
+				TavernaServerImpl.log.warn("failed to close channel", e);
+			}
+		}
+		VERSION = p.getProperty("tavernaserver.version", "unknownVersion");
+		REVISION = p.getProperty("tavernaserver.revision", "unknownRevision")
+				+ " (branch: " + p.getProperty("tavernaserver.branch", "trunk")
+				+ ")";
+		TIMESTAMP = p
+				.getProperty("tavernaserver.timestamp", "unknownTimestamp");
+	}
 
 	public VersionedElement() {
 	}
 
 	protected VersionedElement(boolean ignored) {
 		serverVersion = VERSION;
+		serverRevision = REVISION;
+		serverBuildTimestamp = TIMESTAMP;
 	}
 }
