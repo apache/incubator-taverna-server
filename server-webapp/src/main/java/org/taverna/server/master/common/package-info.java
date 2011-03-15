bfd97d9cfd1ef6877e45d0c38d9d6c78f0f207d0
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-03-15 16:19:04 +0000

    Pin down the namespace local names a bit more strongly

--- server-webapp/src/main/java/org/taverna/server/master/common/package-info.java
+++ server-webapp/src/main/java/org/taverna/server/master/common/package-info.java
@@ -1,7 +1,15 @@
-@XmlSchema(namespace = Namespaces.SERVER, xmlns = { @XmlNs(prefix = "xlink", namespaceURI = Namespaces.XLINK) }, elementFormDefault = QUALIFIED, attributeFormDefault = QUALIFIED)
+@XmlSchema(namespace = Namespaces.SERVER, elementFormDefault = QUALIFIED, attributeFormDefault = QUALIFIED, xmlns = {
+		@XmlNs(prefix = "xlink", namespaceURI = XLINK),
+		@XmlNs(prefix = "ts", namespaceURI = SERVER),
+		@XmlNs(prefix = "ts-rest", namespaceURI = SERVER_REST),
+		@XmlNs(prefix = "ts-soap", namespaceURI = SERVER_SOAP) })
 package org.taverna.server.master.common;
 
 import static javax.xml.bind.annotation.XmlNsForm.QUALIFIED;
+import static org.taverna.server.master.common.Namespaces.SERVER;
+import static org.taverna.server.master.common.Namespaces.SERVER_REST;
+import static org.taverna.server.master.common.Namespaces.SERVER_SOAP;
+import static org.taverna.server.master.common.Namespaces.XLINK;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlSchema;
