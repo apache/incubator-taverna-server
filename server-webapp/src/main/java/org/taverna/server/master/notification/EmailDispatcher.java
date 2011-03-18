1076f2abbaf9c698d3d18f8b9376175c07322c89
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-03-18 12:07:11 +0000

    Soft-code the dependency on JavaMail so things won't be confused by the complex environment of a webapp container.

--- server-webapp/src/main/java/org/taverna/server/master/notification/EmailDispatcher.java
+++ server-webapp/src/main/java/org/taverna/server/master/notification/EmailDispatcher.java
@@ -5,17 +5,12 @@
  */
 package org.taverna.server.master.notification;
 
-import static javax.mail.Message.RecipientType.TO;
-import static javax.mail.Transport.send;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 import static org.taverna.server.master.notification.NotificationEngine.log;
 
+import java.lang.reflect.InvocationTargetException;
+
 import javax.annotation.PostConstruct;
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -32,12 +27,17 @@ public class EmailDispatcher implements MessageDispatcher {
 	/**
 	 * @param from
 	 *            Email address that the notification is to come from.
-	 * @throws AddressException
-	 *             If a bad email address is given.
 	 */
 	@Required
-	public void setFrom(String from) throws AddressException {
-		this.from = new InternetAddress(from);
+	public void setFrom(String from) {
+		try {
+			if (ADDRESS_CLS != null)
+			this.from = ADDRESS_CLS.getConstructor(String.class).newInstance(from);
+		} catch (InvocationTargetException e) {
+			log.warn("failed to construct address", e.getTargetException());
+		} catch (Exception e) {
+			log.warn("failed to make an instance of " + ADDRESS_CLS.getName(), e);
+		}
 	}
 
 	/**
@@ -49,15 +49,30 @@ public class EmailDispatcher implements MessageDispatcher {
 		this.contentType = contentType;
 	}
 
-	private InternetAddress from;
+	private Object from;
 	private String contentType = TEXT_PLAIN;
-
-	private Context env() throws NamingException {
-		return (Context) new InitialContext().lookup("java:comp/env");
+	private static Class<?> SESSION_CLS, MESSAGE_CLS, ADDRESS_CLS, RECIP_CLS,
+			TRANSPORT_CLS;
+	private static Object TO_HDR;
+	static {
+		try {
+			SESSION_CLS = Class.forName("javax.mail.Session");
+			MESSAGE_CLS = Class.forName("javax.mail.internet.MimeMessage");
+			ADDRESS_CLS = Class.forName("javax.mail.internet.InternetAddress");
+			RECIP_CLS = Class.forName("javax.mail.Message.RecipientType");
+			TRANSPORT_CLS = Class.forName("javax.mail.Transport");
+			TO_HDR = RECIP_CLS.getField("TO");
+		} catch (Exception e) {
+			NotificationEngine.log.fatal("failed to look up mail classes", e);
+		}
 	}
 
-	private Session mail() throws NamingException {
-		return (Session) env().lookup("mail/Session");
+	private Object mail() throws NamingException {
+		Context env = (Context) new InitialContext().lookup("java:comp/env");
+		Object o = env.lookup("mail/Session");
+		if (SESSION_CLS.isInstance(o))
+			return o;
+		throw new NamingException("unexpected type?! " + o.getClass());
 	}
 
 	@PostConstruct
@@ -82,20 +97,28 @@ public class EmailDispatcher implements MessageDispatcher {
 			return;
 		}
 
-		Session session = mail();
+		Object session = mail();
 
 		if (session == null)
 			return;
-		Message message = new MimeMessage(session);
-		message.setFrom(from);
-		message.setRecipient(TO, new InternetAddress(to.trim()));
-		message.setSubject(messageSubject);
-		message.setContent(messageContent, contentType);
-		send(message);
+		Object m = MESSAGE_CLS.getConstructor(SESSION_CLS).newInstance(session);
+		MESSAGE_CLS.getMethod("setFrom", ADDRESS_CLS).invoke(m, from);
+		Object realto = ADDRESS_CLS.getConstructor(String.class).newInstance(
+				to.trim());
+		MESSAGE_CLS.getMethod("setRecipient", RECIP_CLS, ADDRESS_CLS).invoke(m,
+				TO_HDR, realto);
+		MESSAGE_CLS.getMethod("setSubject", String.class).invoke(m,
+				messageSubject);
+		MESSAGE_CLS.getMethod("setContent", String.class, String.class).invoke(
+				m, messageContent, contentType);
+		TRANSPORT_CLS.getMethod("send", MESSAGE_CLS).invoke(null, m);
 	}
 
 	@Override
 	public boolean isAvailable() {
+		if (SESSION_CLS == null || MESSAGE_CLS == null || ADDRESS_CLS == null
+				|| RECIP_CLS == null || TRANSPORT_CLS == null || TO_HDR == null)
+			return false;
 		try {
 			return from != null && null != mail();
 		} catch (Exception e) {
