5cba4c3bf35acd56111c18134c30a147817c8fcf
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-02-13 15:41:41 +0000

    Refactoring a bit, adding deletion operation, documenting.

--- server-webapp/src/main/java/org/taverna/server/master/interaction/InteractionFeedSupport.java
+++ server-webapp/src/main/java/org/taverna/server/master/interaction/InteractionFeedSupport.java
@@ -9,11 +9,13 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.Date;
-import java.util.UUID;
+
+import javax.annotation.Resource;
 
 import org.apache.abdera.Abdera;
 import org.apache.abdera.model.Entry;
 import org.apache.abdera.model.Feed;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.taverna.server.master.ContentsDescriptorBuilder.UriBuilderFactory;
 import org.taverna.server.master.TavernaServerSupport;
 import org.taverna.server.master.exceptions.FilesystemAccessException;
@@ -27,13 +29,39 @@ import org.taverna.server.master.interfaces.TavernaRun;
 import org.taverna.server.master.utils.FilenameUtils;
 
 public class InteractionFeedSupport {
+	/**
+	 * The name of the resource within the run resource that is the run's
+	 * interaction feed resource.
+	 */
+	public static final String FEED_URL_DIR = "feed";
+	/**
+	 * The name of the directory below the run working directory that will
+	 * contain the entries of the interaction feed.
+	 */
+	public static final String FEED_DIR = "feed";
+	/**
+	 * Should the contents of the entry be stripped when describing the overall
+	 * feed? This makes sense if (and only if) large entries are being pushed
+	 * through the feed.
+	 */
+	private static final boolean STRIP_CONTENTS = false;
+	/** Maximum size of an entry before truncation. */
+	private static final long MAX_ENTRY_SIZE = 50 * 1024;
+
+	@Autowired(required = true)
 	private TavernaServerSupport support;
+	@Autowired(required = true)
 	private FilenameUtils utils;
+	@Autowired(required = true)
 	private Abdera abdera;
+	@Resource(name = "webapp")
 	private UriBuilderFactory uriBuilder;
 
 	private Entry getEntryFromFile(File f) throws FilesystemAccessException {
-		byte[] contents = f.getContents(0, (int) f.getSize());
+		long size = f.getSize();
+		if (size > MAX_ENTRY_SIZE)
+			throw new FilesystemAccessException("entry larger than 50kB");
+		byte[] contents = f.getContents(0, (int) size);
 		return (Entry) abdera.getParser()
 				.parse(new ByteArrayInputStream(contents)).getRoot();
 	}
@@ -50,43 +78,66 @@ public class InteractionFeedSupport {
 		f.appendContents(baos.toByteArray());
 	}
 
-	public Feed getRunFeed(String runID) throws FilesystemAccessException,
-			NoDirectoryEntryException, UnknownRunException,
-			MalformedURLException {
-		TavernaRun run = support.getRun(runID);
-		Directory feedDir = utils.getDirectory(run, "feed");
+	/**
+	 * Get the interaction feed for a partciular run.
+	 * 
+	 * @param run
+	 *            The run to get the feed of.
+	 * @return The Abdera feed descriptor.
+	 * @throws FilesystemAccessException
+	 *             If the feed directory can't be read for some reason.
+	 * @throws NoDirectoryEntryException
+	 *             If the feed directory doesn't exist or an entry is
+	 *             unexpectedly removed.
+	 */
+	public Feed getRunFeed(TavernaRun run) throws FilesystemAccessException,
+			NoDirectoryEntryException {
+		Directory feedDir = utils.getDirectory(run, FEED_DIR);
 		URI feedURI = uriBuilder.getRunUriBuilder(run).path("feed").build();
 		Feed feed = abdera.newFeed();
-		feed.setTitle("Interactions for Taverna Run #" + runID);
-		feed.addLink(feedURI.toURL().toString(), "self");
+		feed.setTitle("Interactions for Taverna Run #" + run.getId());
+		try {
+			feed.addLink(feedURI.toURL().toString(), "self");
+		} catch (MalformedURLException e) {
+			// Should be impossible; ignore...
+		}
 		Date d = null;
 		for (DirectoryEntry de : feedDir.getContentsByDate()) {
 			if (!(de instanceof File))
 				continue;
-			Entry e = getEntryFromFile((File) de);
-			// TODO Should the contents of the entry be stripped?
-			//e.setContentElement(null);
-			feed.addEntry(e);
-			d = de.getModificationDate();
+			try {
+				Entry e = getEntryFromFile((File) de);
+				if (STRIP_CONTENTS)
+					e.setContentElement(null);
+				feed.addEntry(e);
+				d = de.getModificationDate();
+			} catch (FilesystemAccessException e) {
+				// Can't do anything about it, so we'll just drop the entry.
+			}
 		}
-		feed.setUpdated(d);
+		if (d != null)
+			feed.setUpdated(d);
 		return feed;
 	}
 
 	/**
 	 * Gets the contents of a particular feed entry.
-	 * @param runID
+	 * 
+	 * @param run
+	 *            The workflow run within which context we are operating.
 	 * @param entryID
-	 * @return
+	 *            The identifier (from the path) of the entry to read.
+	 * @return The description of the entry.
 	 * @throws FilesystemAccessException
+	 *             If the entry can't be read or is too large.
 	 * @throws NoDirectoryEntryException
-	 * @throws UnknownRunException
+	 *             If the entry can't be found.
 	 */
-	public Entry getRunFeedEntry(String runID, String entryID)
+	public Entry getRunFeedEntry(TavernaRun run, String entryID)
 			throws FilesystemAccessException, NoDirectoryEntryException,
 			UnknownRunException {
-		File entryFile = utils.getFile(support.getRun(runID), "feed/" + entryID
-				+ ".entry");
+		File entryFile = utils
+				.getFile(run, FEED_DIR + "/" + entryID + ".entry");
 		return getEntryFromFile(entryFile);
 	}
 
@@ -95,8 +146,8 @@ public class InteractionFeedSupport {
 	 * for a particular run. Note that this does not permit update of an
 	 * existing entry; the entry is always created new.
 	 * 
-	 * @param runID
-	 *            The ID of the run.
+	 * @param run
+	 *            The workflow run within which context we are operating.
 	 * @param entry
 	 *            The partial entry to store
 	 * @return A link to the entry.
@@ -104,17 +155,14 @@ public class InteractionFeedSupport {
 	 *             If the entry can't be stored.
 	 * @throws NoDirectoryEntryException
 	 *             If the run is improperly configured.
-	 * @throws UnknownRunException
-	 *             If the run is unknown.
 	 * @throws NoUpdateException
 	 *             If the user isn't allowed to do the write.
 	 * @throws MalformedURLException
 	 *             If a generated URL is illegal (shouldn't happen).
 	 */
-	public String addRunFeedEntry(String runID, Entry entry)
+	public String addRunFeedEntry(TavernaRun run, Entry entry)
 			throws FilesystemAccessException, NoDirectoryEntryException,
-			UnknownRunException, NoUpdateException, MalformedURLException {
-		TavernaRun run = support.getRun(runID);
+			NoUpdateException, MalformedURLException {
 		support.permitUpdate(run);
 		// TODO Should this id be generated like this?
 		String localId = "interact_" + currentTimeMillis();
@@ -123,8 +171,29 @@ public class InteractionFeedSupport {
 				.path("feed/{entryID}").build(localId).toURL().toString();
 		entry.addLink(selfLink);
 		entry.setUpdated(new Date());
-		putEntryInFile(utils.getDirectory(run, "feed"), localId + ".entry",
+		putEntryInFile(utils.getDirectory(run, FEED_DIR), localId + ".entry",
 				entry);
 		return selfLink;
+	}
+
+	/**
+	 * 
+	 * @param run
+	 *            The workflow run within which context we are operating.
+	 * @param entryID
+	 *            The ID of the entry.
+	 * @throws FilesystemAccessException
+	 *             If the entry can't be deleted
+	 * @throws NoDirectoryEntryException
+	 *             If the entry can't be found.
+	 * @throws NoUpdateException
+	 *             If the current user is not permitted to modify the run's
+	 *             characteristics.
+	 */
+	public void removeRunFeedEntry(TavernaRun run, String entryID)
+			throws FilesystemAccessException, NoDirectoryEntryException,
+			NoUpdateException {
+		support.permitUpdate(run);
+		utils.getFile(run, FEED_DIR + "/" + entryID + ".entry").destroy();
 	}
 }
