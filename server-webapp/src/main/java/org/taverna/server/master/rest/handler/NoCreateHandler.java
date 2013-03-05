0d1a102b676b51d3f4a19b5660472caea4577e66
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-03-05 14:42:35 +0000

    Corrected HTTP result code for "unable to make new run"

--- server-webapp/src/main/java/org/taverna/server/master/rest/handler/NoCreateHandler.java
+++ server-webapp/src/main/java/org/taverna/server/master/rest/handler/NoCreateHandler.java
@@ -5,7 +5,7 @@
  */
 package org.taverna.server.master.rest.handler;
 
-import static javax.ws.rs.core.Response.Status.FORBIDDEN;
+import static javax.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -18,6 +18,6 @@ public class NoCreateHandler extends HandlerCore implements
 		ExceptionMapper<NoCreateException> {
 	@Override
 	public Response toResponse(NoCreateException exn) {
-		return respond(FORBIDDEN, exn);
+		return respond(SERVICE_UNAVAILABLE, exn);
 	}
 }
