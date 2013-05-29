50c68753c777f3623f19d3e054f38e29596fd21e
dkf <donal.k.fellows@manchester.ac.uk>
2013-05-29 11:53:56 +0100

    Name generation failure should not block run creation.

--- server-webapp/src/main/java/org/taverna/server/master/TavernaServerSupport.java
+++ server-webapp/src/main/java/org/taverna/server/master/TavernaServerSupport.java
@@ -589,7 +589,13 @@ public class TavernaServerSupport {
 			TavernaSecurityContext c = run.getSecurityContext();
 			c.initializeSecurityFromContext(SecurityContextHolder.getContext());
 			webapp.initObsoleteSecurity(c);
-			run.setName(workflow.getName() + " " + run.getCreationTimestamp());
+			String name = "";
+			try {
+				name = workflow.getName() + " " + run.getCreationTimestamp();
+			} catch (Exception e) {
+				// Ignore; it's just a name, not something important.
+			}
+			run.setName(name);
 		} catch (Exception e) {
 			log.error("failed to build workflow run worker", e);
 			throw new NoCreateException("failed to build workflow run worker");
