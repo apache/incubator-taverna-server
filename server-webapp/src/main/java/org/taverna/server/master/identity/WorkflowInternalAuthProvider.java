b77404571878bb87b3a7bcb1acbda8aa4a47ee2e
dkf <donal.k.fellows@manchester.ac.uk>
2013-06-29 10:55:10 +0100

    Refactor a bit.

--- server-webapp/src/main/java/org/taverna/server/master/identity/WorkflowInternalAuthProvider.java
+++ server-webapp/src/main/java/org/taverna/server/master/identity/WorkflowInternalAuthProvider.java
@@ -87,65 +87,123 @@ public class WorkflowInternalAuthProvider extends
 		authorizedAddresses = new HashSet<String>(localAddresses);
 	}
 
-	@Override
+    /**
+	 * Check that the authentication request is actually valid for the given
+	 * user record.
+	 * 
+	 * @param userRecord
+	 *            as retrieved from the
+	 *            {@link #retrieveUser(String, UsernamePasswordAuthenticationToken)}
+	 *            or <code>UserCache</code>
+	 * @param details
+	 *            the details of the authentication
+	 * @param principal
+	 *            the principal that is trying to authenticate (and that we're
+	 *            trying to bind)
+	 * @param credentials
+	 *            the credentials (e.g., password) presented by the principal
+	 * 
+	 * @throws AuthenticationException
+	 *             AuthenticationException if the credentials could not be
+	 *             validated (generally a <code>BadCredentialsException</code>,
+	 *             an <code>AuthenticationServiceException</code>)
+	 * @throws Exception
+	 *             If something goes wrong. Will be logged and converted to a
+	 *             generic AuthenticationException.
+	 */
 	protected void additionalAuthenticationChecks(UserDetails userRecord,
+			Object details, Object principal, Object credentials)
+			throws Exception {
+		WebAuthenticationDetails wad = (WebAuthenticationDetails) details;
+		HttpServletRequest req = ((ServletRequestAttributes) getRequestAttributes())
+				.getRequest();
+
+		// Are we coming from a "local" address?
+		if (!req.getLocalAddr().equals(wad.getRemoteAddress())
+				&& !authorizedAddresses.contains(wad.getRemoteAddress())) {
+			if (logDecisions)
+				log.info("attempt to use workflow magic token from untrusted address:"
+						+ " token=" + userRecord.getUsername()
+						+ ", address=" + wad.getRemoteAddress());
+			throw new BadCredentialsException("bad login token");
+		}
+
+		// Does the password match?
+		if (!credentials.equals(userRecord.getPassword())) {
+			if (logDecisions)
+				log.info("workflow magic token is untrusted due to password mismatch:"
+						+ " wanted=" + userRecord.getPassword()
+						+ ", got=" + credentials);
+			throw new BadCredentialsException("bad login token");
+		}
+
+		if (logDecisions)
+			log.info("granted role " + SELF + " to user "
+					+ userRecord.getUsername());
+	}
+
+    /**
+	 * Retrieve the <code>UserDetails</code> from the relevant store, with the
+	 * option of throwing an <code>AuthenticationException</code> immediately if
+	 * the presented credentials are incorrect (this is especially useful if it
+	 * is necessary to bind to a resource as the user in order to obtain or
+	 * generate a <code>UserDetails</code>).
+	 * 
+	 * @param username
+	 *            The username to retrieve
+	 * @param details
+	 *            The details from the authentication request.
+	 * @see #retrieveUser(String,UsernamePasswordAuthenticationToken)
+	 * @return the user information (never <code>null</code> - instead an
+	 *         exception should the thrown)
+	 * @throws AuthenticationException
+	 *             if the credentials could not be validated (generally a
+	 *             <code>BadCredentialsException</code>, an
+	 *             <code>AuthenticationServiceException</code> or
+	 *             <code>UsernameNotFoundException</code>)
+	 * @throws Exception
+	 *             If something goes wrong. It will be logged and converted into
+	 *             a general AuthenticationException.
+	 */
+	@NonNull
+	protected UserDetails retrieveUser(String username, Object details) throws Exception {
+		if (details == null || !(details instanceof WebAuthenticationDetails))
+			throw new UsernameNotFoundException("context unsupported");
+		if (!username.startsWith(PREFIX))
+			throw new UsernameNotFoundException(
+					"unsupported username for this provider");
+		if (logDecisions)
+			log.info("request for auth for user " + username);
+		String wfid = username.substring(PREFIX.length());
+		String securityToken = dao.getSecurityToken(wfid);
+		if (securityToken == null)
+			throw new UsernameNotFoundException("no such user");
+		return new User(username, securityToken, true, true, true, true,
+				Arrays.asList(new LiteralGrantedAuthority(SELF),
+						new WorkflowSelfAuthority(wfid)));
+	}
+
+	@Override
+	protected final void additionalAuthenticationChecks(UserDetails userRecord,
 			UsernamePasswordAuthenticationToken token) {
 		try {
-			WebAuthenticationDetails wad = (WebAuthenticationDetails) token
-					.getDetails();
-			HttpServletRequest req = ((ServletRequestAttributes) getRequestAttributes())
-					.getRequest();
-
-			// Are we coming from a "local" address?
-			if (!req.getLocalAddr().equals(wad.getRemoteAddress())
-					&& !authorizedAddresses.contains(wad.getRemoteAddress())) {
-				if (logDecisions)
-					log.info("attempt to use workflow magic token from untrusted address:"
-							+ " token=" + userRecord.getUsername()
-							+ ", address=" + wad.getRemoteAddress());
-				throw new BadCredentialsException("bad login token");
-			}
-
-			// Does the password match?
-			if (!token.getCredentials().equals(userRecord.getPassword())) {
-				if (logDecisions)
-					log.info("workflow magic token is untrusted due to password mismatch:"
-							+ " wanted=" + userRecord.getPassword()
-							+ ", got=" + token.getCredentials());
-				throw new BadCredentialsException("bad login token");
-			}
-
-			if (logDecisions)
-				log.info("granted role " + SELF + " to user "
-						+ userRecord.getUsername());
+			additionalAuthenticationChecks(userRecord, token.getDetails(),
+					token.getPrincipal(), token.getCredentials());
 		} catch (AuthenticationException e) {
 			throw e;
 		} catch (Exception e) {
 			log.warn("unexpected failure in authentication", e);
-			throw new AuthenticationServiceException("unexpected failure in authentication", e);
+			throw new AuthenticationServiceException(
+					"unexpected failure in authentication", e);
 		}
 	}
 
 	@Override
 	@NonNull
-	protected UserDetails retrieveUser(String username,
+	protected final UserDetails retrieveUser(String username,
 			UsernamePasswordAuthenticationToken token) {
 		try {
-			if (token.getDetails() == null
-					|| !(token.getDetails() instanceof WebAuthenticationDetails))
-				throw new UsernameNotFoundException("context unsupported");
-			if (!username.startsWith(PREFIX))
-				throw new UsernameNotFoundException(
-						"unsupported username for this provider");
-			if (logDecisions)
-				log.info("request for auth for user " + username);
-			String wfid = username.substring(PREFIX.length());
-			String securityToken = dao.getSecurityToken(wfid);
-			if (securityToken == null)
-				throw new UsernameNotFoundException("no such user");
-			return new User(username, securityToken, true, true, true, true,
-					Arrays.asList(new LiteralGrantedAuthority(SELF),
-							new WorkflowSelfAuthority(wfid)));
+			return retrieveUser(username, token.getDetails());
 		} catch (AuthenticationException e) {
 			throw e;
 		} catch (Exception e) {
