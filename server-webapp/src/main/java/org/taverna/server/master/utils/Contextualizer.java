3f874d3c7de338bf745c362430ed92cc8d948a66
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2013-12-11 11:57:54 +0000

    Corrected the URL presented in the front facade.

--- server-webapp/src/main/java/org/taverna/server/master/utils/Contextualizer.java
+++ server-webapp/src/main/java/org/taverna/server/master/utils/Contextualizer.java
@@ -6,6 +6,7 @@
 package org.taverna.server.master.utils;
 
 import javax.servlet.ServletContext;
+import javax.ws.rs.core.UriInfo;
 
 import org.springframework.web.context.ServletContextAware;
 import org.taverna.server.master.common.version.Version;
@@ -19,6 +20,7 @@ import org.taverna.server.master.common.version.Version;
 public class Contextualizer implements ServletContextAware {
 	static final String ROOT_PLACEHOLDER = "%{WEBAPPROOT}";
 	static final String VERSION_PLACEHOLDER = "%{VERSION}";
+	static final String BASE_PLACEHOLDER = "%{BASEURL}";
 
 	/**
 	 * Apply the contextualization operation. This consists of replacing the
@@ -33,6 +35,26 @@ public class Contextualizer implements ServletContextAware {
 		String path = context.getRealPath("/").replace("%2D", "-");
 		return input.replace(ROOT_PLACEHOLDER, path).replace(
 				VERSION_PLACEHOLDER, Version.JAVA);
+	}
+
+	/**
+	 * Apply the contextualization operation. This consists of replacing the
+	 * string <tt>{@value #ROOT_PLACEHOLDER}</tt> with the real root of the
+	 * webapp.
+	 * 
+	 * @param ui
+	 *            Where to get information about the URL used to access the
+	 *            webapp.
+	 * @param input
+	 *            the string to contextualize
+	 * @return the contextualized string
+	 */
+	public String contextualize(UriInfo ui, String input) {
+		// Hack to work around bizarre CXF bug
+		String baseuri = ui.getBaseUri().toString().replace("%2D", "-");
+		if (baseuri.endsWith("/"))
+			baseuri = baseuri.substring(0, baseuri.length() - 1);
+		return contextualize(input).replace(BASE_PLACEHOLDER, baseuri);
 	}
 
 	private ServletContext context;
