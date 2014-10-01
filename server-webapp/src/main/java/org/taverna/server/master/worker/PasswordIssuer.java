8aa9648620d05cd1ca8b669d03f0a6631063ee8e
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2014-10-01 14:33:31 +0100

    TAV-520: Reduce the size of the symbol set used for passwords to ensure that they cannot be misinterpreted by anything.

--- server-webapp/src/main/java/org/taverna/server/master/worker/PasswordIssuer.java
+++ server-webapp/src/main/java/org/taverna/server/master/worker/PasswordIssuer.java
@@ -17,9 +17,7 @@ public class PasswordIssuer {
 			'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
 			'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
 			'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7',
-			'8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
-			',', '.', '<', '>', '/', '?', ':', ';', '-', '_', '+', '[', ']',
-			'{', '}', '`', '~' };
+			'8', '9', '0', '_' };
 	private Log log = LogFactory.getLog("Taverna.Server.Worker");
 	private SecureRandom r;
 	private int length;
