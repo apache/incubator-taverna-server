419a7d460e0f509174a8370187c203500208456d
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2016-09-09 15:54:05 +0100

    Fixing licenses and some RAT excludes.

--- src/clients/tcl/taverna2server.tcl
+++ src/clients/tcl/taverna2server.tcl
@@ -1,3 +1,18 @@
+##  Licensed to the Apache Software Foundation (ASF) under one or more
+##  contributor license agreements.  See the NOTICE file distributed with
+##  this work for additional information regarding copyright ownership.
+##  The ASF licenses this file to You under the Apache License, Version 2.0
+##  (the "License"); you may not use this file except in compliance with
+##  the License.  You may obtain a copy of the License at
+##
+##      http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+
 # Tcl package providing access to Taverna 2 Server release 1.
 # This code also works as an executable script. Use it like this:
 #
@@ -13,11 +28,7 @@
 #    Tcl    8.6
 #    tdom   0.8.2
 #
-# Copyright (c) 2010, The University of Manchester
-#
-# $Id$
-#
-
+
 if {[package vsatisfies [package require Tcl 8.5] 8.6]} {
     # Skip requiring features that are built in to 8.6
 } else {
